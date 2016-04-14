60950bd21b4f5e8922ad3414208174a68cb115db
rafaelpernil2 <rafaelpernil@uma.es>
2016-04-14 18:05:26 +0200

    My Base Code

---
+++ GeneticBattle/src/battle/Battle.java
@@ -0,0 +1,24 @@
+package battle;
+
+import robocode.control.*;
+
+public class Battle {
+	private BattlefieldSpecification battlefieldSize;
+	private int numRounds, sentryBorderSize;
+	private long inactivityTime;
+	private double gunCoolingRate;
+	private boolean hideEnemyNames;
+	private RobotSpecification[] robots;
+
+	public Battle(BattlefieldSpecification battlefieldSize, int numRounds, long inactivityTime, double gunCoolingRate,
+			int sentryBorderSize, boolean hideEnemyNames, RobotSpecification[] robots) {
+		this.battlefieldSize = battlefieldSize;
+		this.numRounds = numRounds;
+		this.inactivityTime = inactivityTime;
+		this.sentryBorderSize = sentryBorderSize;
+		this.gunCoolingRate = gunCoolingRate;
+		this.hideEnemyNames = hideEnemyNames;
+		this.robots = robots;
+	}
+
+}
