d68d2b12d1ce8d303da73ce8fa3eecf8c6e8c52d
Rafael Pernil <rafaelpernil@uma.es>
2016-05-10 04:11:55 +0200

    Working version v1.1 (Added a few tweaks)

--- SuperTracker/src/supersample/DefBattle.java
+++ SuperTracker/src/supersample/DefBattle.java
@@ -24,61 +24,27 @@ import robocode.control.*;
 public class DefBattle {
 	public static void main(String[] args) {
 
-		RobocodeEngine engine = new RobocodeEngine(new java.io.File("C:/robocode"));
-		engine.setVisible(true);
-		int NumPixelRows = 832;
-		int NumPixelCols = 640;
-		BattlefieldSpecification battlefield = new BattlefieldSpecification(NumPixelRows, NumPixelCols);
-		// 800x600
-		// Setup battle parameters
-		int numberOfRounds = 10;
-		long inactivityTime = 10000000;
-		double gunCoolingRate = 1.0;
-		int sentryBorderSize = 50;
-		boolean hideEnemyNames = false;
-		int NumEnemies = 1; // Defined by programmer
-		// Define specifications
-		RobotSpecification[] modelRobots = engine
-				.getLocalRepository("supersample.SuperRamFire*,supersample.SuperTracker*");
-		RobotSpecification[] existingRobots = new RobotSpecification[NumEnemies + 1];
-		RobotSetup[] robotSetups = new RobotSetup[NumEnemies + 1];
-		existingRobots[NumEnemies] = modelRobots[1];
-		existingRobots[0] = modelRobots[0];
-		robotSetups[0] = new RobotSetup((double) (1 * 64 + 32), (double) (5 * 64 + 32), 0.0);
-		double InitialAgentRow = (5 * 64) + 32;
-		double InitialAgentCol = (11 * 64) + 32;
-		robotSetups[NumEnemies] = new RobotSetup(InitialAgentCol, InitialAgentRow, 0.0);
-
-		/* Create and run the battle */
-
-		BattleSpecification battleSpec = new BattleSpecification(battlefield, numberOfRounds, inactivityTime,
-				gunCoolingRate, sentryBorderSize, hideEnemyNames, existingRobots, robotSetups);
-
-		// Run our specified battle and let it run till it is over
-		engine.runBattle(battleSpec, true); // waits till the battle finishes
-		// Cleanup our RobocodeEngine
-		engine.close();
-
-		int ramfire = 0, supertracker = 0;
-		try {
-			Scanner sc = new Scanner(new java.io.File(
-					"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperRamFire.data/score.txt"));
-			if (sc.hasNext()) {
-				ramfire = sc.nextInt();
-			}
-			sc.close();
-			Scanner sc2 = new Scanner(new java.io.File(
-					"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/score.txt"));
-			if (sc2.hasNext()) {
-				supertracker = sc2.nextInt();
-			}
-			sc2.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(ramfire);
-		System.out.println(supertracker);
+		
+//		int ramfire = 0, supertracker = 0;
+//		try {
+//			Scanner sc = new Scanner(new java.io.File(
+//					"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperRamFire.data/score.txt"));
+//			if (sc.hasNext()) {
+//				ramfire = sc.nextInt();
+//			}
+//			sc.close();
+//			Scanner sc2 = new Scanner(new java.io.File(
+//					"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/score.txt"));
+//			if (sc2.hasNext()) {
+//				supertracker = sc2.nextInt();
+//			}
+//			sc2.close();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.println(ramfire);
+//		System.out.println(supertracker);
 
 //		try {
 //			PrintStream w = new PrintStream(new FileOutputStream(new File(
@@ -114,17 +80,16 @@ public class DefBattle {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		RandomGenerator randomGenerator = null;
 		try {
 			DefaultConfiguration config = new DefaultConfiguration();
 			DoubleGene[] genes = new DoubleGene[4];
-			genes[0] = new DoubleGene (config, 0.0, 1500);
+			genes[0] = new DoubleGene (config, 0.0, 832);
 			genes[0].setAllele(150.0);
-			genes[1] = new DoubleGene (config, 0.0, 0.9999);
+			genes[1] = new DoubleGene (config, 0.0, 0.999999);
 			genes[1].setAllele(0.1);
-			genes[2] = new DoubleGene (config, 0.0, 1500);
+			genes[2] = new DoubleGene (config, 0.0, 150);
 			genes[2].setAllele(12.0);
-			genes[3] = new DoubleGene (config, 0.0, 1500);
+			genes[3] = new DoubleGene (config, 0.0, 150);
 			genes[3].setAllele(12.0);
 			IChromosome pgp = new Chromosome(genes);
 			OurFitnessFunction fit = new OurFitnessFunction (pgp.getFitnessValue());
@@ -137,7 +102,7 @@ public class DefBattle {
 				genes[3].applyMutation(0, 1-evaluate);
 				double evaluate2 = fit.evaluate(pgp);
 				pgp.setFitnessValue(Math.max(evaluate, evaluate2));
-				System.out.println("Calculated" + i);
+				System.out.println("Calculated " + i);
 			}
 			System.out.println("Ended");
 		} catch (InvalidConfigurationException e) {
--- SuperTracker/src/supersample/OurFitnessFunction.java
+++ SuperTracker/src/supersample/OurFitnessFunction.java
@@ -1,15 +1,10 @@
 package supersample;
 
-import java.awt.List;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-import org.jgap.Chromosome;
 import org.jgap.FitnessFunction;
 import org.jgap.IChromosome;
-import robocode.*;
 import robocode.control.BattleSpecification;
 import robocode.control.BattlefieldSpecification;
 import robocode.control.RobocodeEngine;
@@ -32,6 +27,7 @@ import robocode.control.RobotSpecification;
  */
 @SuppressWarnings("serial")
 public class OurFitnessFunction extends FitnessFunction {
+	@SuppressWarnings(value = { "unused" })
 	private final double my_fitness_percentage;
 
 	public OurFitnessFunction(double fitness_percentage) {
@@ -54,111 +50,100 @@ public class OurFitnessFunction extends FitnessFunction {
 	 *         Chromosome.
 	 */
 	protected double evaluate(IChromosome a_subject) {
-		//Read genes
-		double [] results = new double [10];
-		for (int i=0; i < 10; i++){
-		double distance_limit = (double) a_subject.getGene(0).getAllele();
-		double speed_change_probability = (double) a_subject.getGene(1).getAllele();
-		double range_of_speeds = (double) a_subject.getGene(2).getAllele();
-		double min_robot_speed = (double) a_subject.getGene(3).getAllele();
-		//Write file
-		try {
-			PrintStream w = new PrintStream(new FileOutputStream(new File("C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/genes.txt")));
-			w.println(distance_limit);
-			w.println(speed_change_probability);
-			w.println(range_of_speeds);
-			w.println(min_robot_speed);
-			// PrintStreams don't throw IOExceptions during prints, they simply
-			// set a flag.... so check it here.
-			if (w.checkError()) {
-				System.out.println("I could not write!");
+		// Read genes
+		final int MAX = 10;
+		double sum = 0.0;
+		double[] results = new double[MAX];
+		for (int i = 0; i < MAX; i++) {
+			double distance_limit = (double) a_subject.getGene(0).getAllele();
+			double speed_change_probability = (double) a_subject.getGene(1).getAllele();
+			double range_of_speeds = (double) a_subject.getGene(2).getAllele();
+			double min_robot_speed = (double) a_subject.getGene(3).getAllele();
+			// Write file
+			try {
+				PrintStream w = new PrintStream(new FileOutputStream(new File(
+						"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/genes.txt")));
+				w.println(distance_limit);
+				w.println(speed_change_probability);
+				w.println(range_of_speeds);
+				w.println(min_robot_speed);
+				// PrintStreams don't throw IOExceptions during prints, they
+				// simply
+				// set a flag.... so check it here.
+				if (w.checkError()) {
+					System.out.println("I could not write!");
+				}
+				if (w != null) {
+					w.close();
+				}
+			} catch (IOException ex) {
+				System.out.println("IOException trying to write: ");
+				ex.printStackTrace(System.out);
 			}
-			if (w != null) {
-				w.close();
+
+			// Within 10 iterations...
+
+			// Run Battle
+			RobocodeEngine engine = new RobocodeEngine(new java.io.File("C:/robocode"));
+			engine.setVisible(false);
+			int NumPixelRows = 832;
+			int NumPixelCols = 640;
+			BattlefieldSpecification battlefield = new BattlefieldSpecification(NumPixelRows, NumPixelCols);
+			// 800x600
+			// Setup battle parameters
+			int numberOfRounds = 10;
+			long inactivityTime = 1;
+			double gunCoolingRate = 1.0;
+			int sentryBorderSize = 50;
+			boolean hideEnemyNames = false;
+			int NumEnemies = 1; // Defined by programmer
+			// Define specifications
+			RobotSpecification[] modelRobots = engine
+					.getLocalRepository("supersample.SuperRamFire*,supersample.SuperTracker*");
+			RobotSpecification[] existingRobots = new RobotSpecification[NumEnemies + 1];
+			RobotSetup[] robotSetups = new RobotSetup[NumEnemies + 1];
+			existingRobots[NumEnemies] = modelRobots[1];
+			existingRobots[0] = modelRobots[0];
+			robotSetups[0] = new RobotSetup((double) (1 * 64 + 32), (double) (5 * 64 + 32), 0.0);
+			double InitialAgentRow = (5 * 64) + 32;
+			double InitialAgentCol = (11 * 64) + 32;
+			robotSetups[NumEnemies] = new RobotSetup(InitialAgentCol, InitialAgentRow, 0.0);
+
+			/* Create and run the battle */
+
+			BattleSpecification battleSpec = new BattleSpecification(battlefield, numberOfRounds, inactivityTime,
+					gunCoolingRate, sentryBorderSize, hideEnemyNames, existingRobots, robotSetups);
+
+			// Run our specified battle and let it run till it is over
+			engine.runBattle(battleSpec, true); // waits till the battle
+												// finishes
+			// Cleanup our RobocodeEngine
+			engine.close();
+
+			// Read battle results
+			int ramfire = 0, supertracker = 0;
+			try {
+				Scanner sc = new Scanner(new java.io.File(
+						"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperRamFire.data/score.txt"));
+				if (sc.hasNext()) {
+					ramfire = sc.nextInt();
+				}
+				sc.close();
+				Scanner sc2 = new Scanner(new java.io.File(
+						"C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/score.txt"));
+				if (sc2.hasNext()) {
+					supertracker = sc2.nextInt();
+				}
+				sc2.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (IOException ex) {
-			System.out.println("IOException trying to write: ");
-			ex.printStackTrace(System.out);
+			// Compare values
+			double fitness = supertracker / (ramfire + supertracker);
+			sum += fitness;
 		}
-		
-		
-		//Within 10 iterations...
-		
-		//Run Battle
-		RobocodeEngine engine = new RobocodeEngine(new java.io.File("C:/robocode"));
-		engine.setVisible(false);
-		int NumPixelRows = 832;
-		int NumPixelCols = 640;
-		BattlefieldSpecification battlefield = new BattlefieldSpecification(NumPixelRows, NumPixelCols);
-		// 800x600
-		// Setup battle parameters
-		int numberOfRounds = 10;
-		long inactivityTime = 10000000;
-		double gunCoolingRate = 1.0;
-		int sentryBorderSize = 50;
-		boolean hideEnemyNames = false;
-		int NumEnemies = 1; // Defined by programmer
-		// Define specifications
-		RobotSpecification[] modelRobots = engine.getLocalRepository("supersample.SuperRamFire*,supersample.SuperTracker*");
-		RobotSpecification[] existingRobots = new RobotSpecification[NumEnemies + 1];
-		RobotSetup[] robotSetups = new RobotSetup[NumEnemies + 1];
-		existingRobots[NumEnemies] = modelRobots[1];
-		existingRobots[0] = modelRobots[0];
-		robotSetups[0] = new RobotSetup((double)(1*64+32),(double)(5*64+32), 0.0);
-		double InitialAgentRow = (5*64)+32;
-		double InitialAgentCol = (11*64)+32;
-		robotSetups[NumEnemies] = new RobotSetup(InitialAgentCol, InitialAgentRow, 0.0);
-
-		/* Create and run the battle */
-
-		BattleSpecification battleSpec = new BattleSpecification(battlefield, numberOfRounds, inactivityTime,
-				gunCoolingRate, sentryBorderSize, hideEnemyNames, existingRobots, robotSetups);
-
-		// Run our specified battle and let it run till it is over
-		engine.runBattle(battleSpec, true); // waits till the battle finishes
-		// Cleanup our RobocodeEngine
-		engine.close();
-		
-		
-		
-		
-		//Read battle results
-		int ramfire=0, supertracker=0;
-		try {
-			Scanner sc = new Scanner(new java.io.File ("C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperRamFire.data/score.txt"));
-			if (sc.hasNext()){
-				ramfire = sc.nextInt();
-			}
-			sc.close();
-			Scanner sc2 = new Scanner(new java.io.File ("C:/Users/rafae/git/PerGamPer-Chromosome/SuperTracker/bin/supersample/SuperTracker.data/score.txt"));
-			if (sc2.hasNext()){
-				supertracker = sc2.nextInt();
-			}
-			sc2.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//Compare values
-		double fitness = supertracker / (ramfire+supertracker);
-		results[i] = fitness;
-		}
-		double max = 0.0;
-		for (int counter = 0; counter < results.length; counter++)
-		{
-		     if (results[counter] > max)
-		     {
-		      max = results[counter];
-		     }
-		}
-
-		//Generate Fitness
-		return max;
+		// Generate Fitness
+		return (sum / results.length);
 	}
-
-//	@Override
-//	protected double evaluate(IChromosome a_subject) {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
 }
